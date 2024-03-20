f7c3454320a7426617c76a067b4d0f15d2b694da
Lucas Soares <lucas.soares.araujo@hotmail.com>
2024-03-20 02:14:16 -0300

    ajusta nome de servico

--- scripts/ecs/unix/build.sh
+++ scripts/ecs/unix/build.sh
@@ -1,5 +1,5 @@
 ECR_REGISTRY="SEU_REGISTRY"
 aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin $ECR_REGISTRY
 docker build -t ms-orders .
-docker tag bia:latest $ECR_REGISTRY/ms-orders:latest
+docker tag ms-orders:latest $ECR_REGISTRY/ms-orders:latest
 docker push $ECR_REGISTRY/ms-orders:latest
